460adc2765134bc64ae1b79d933ee6d46ef5a281
gcolacurcio21 <gcolacurcio21@gmail.com>
2019-09-04 17:05:35 -0300

    Changed the Layout component to a class component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // HOC
 import Aux from '../../hoc/Auxilliary';
@@ -10,13 +10,21 @@ import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => 
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
+class Layout extends Component { 
 
-export default layout;
+    sideDrawerClosedHandler = () => {
+        
+    }
+
+    render() {
+        return <Aux>
+            <Toolbar />
+            <SideDrawer />
+            <main className={classes.Content}>
+                {this.props.children}
+            </main>
+        </Aux>
+    }
+}
+
+export default Layout;
