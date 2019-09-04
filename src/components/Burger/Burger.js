15a7345f74bca8c5846c2df9f9d76048d501f99d
gcolacurcio21 <gcolacurcio21@gmail.com>
2019-09-04 11:14:33 -0300

    Added a message if the ingredients are empty

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -10,14 +10,23 @@ import BurgerIngredient from './BurguerIngredient/BurgerIngredient';
 const burguer = (props) => {
 
     //Make an array of the keys of ingredients
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
+        // For each key in the ingredients we create an array that has x 
+        // elements being x the value of the igKey
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey+i} type={igKey} />
             })
-        }); 
+        // In order to only have one array of items
+        }).reduce((arr, el) => { 
+            return arr.concat(el);
+        }, []); 
 
     console.log(transformedIngredients);
+
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
 
     return (
         <div className={classes.Burger}>
