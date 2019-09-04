144104f60bc32d620447b7db519bdceb061755ae
gcolacurcio21 <gcolacurcio21@gmail.com>
2019-09-04 11:03:31 -0300

    Added the functionality in order that Burguer renders the ingredients set in the state of the BurguerBuilder

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,13 +5,24 @@ import classes from './Burger.css';
 
 // Components
 import BurguerIngredient from './BurguerIngredient/BurgerIngredient';
+import BurgerIngredient from './BurguerIngredient/BurgerIngredient';
 
 const burguer = (props) => {
+
+    //Make an array of the keys of ingredients
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey+i} type={igKey} />
+            })
+        }); 
+
+    console.log(transformedIngredients);
+
     return (
         <div className={classes.Burger}>
             <BurguerIngredient type='bread-top' />
-            <BurguerIngredient type='cheese' />
-            <BurguerIngredient type='meat' />
+            {transformedIngredients}
             <BurguerIngredient type='bread-bottom' />
         </div>
     );
